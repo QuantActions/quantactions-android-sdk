c9142227ca75f2a7b00aac6bec667c3694aa687a
Enea Ceolini <enea.ceolini@quantactions.com>
2025-02-25 09:48:08 +0100

    feat: added first PVT response

---
+++ qa_sdk/src/main/java/com/quantactions/sdk/cognitive_tests/CognitiveTests.kt
@@ -0,0 +1,14 @@
+/*
+ * *******************************************************************************
+ * Copyright (C) QuantActions AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Enea Ceolini <enea.ceolini@quantactions.com>, August 2024
+ * *******************************************************************************
+ */
+
+package com.quantactions.sdk.cognitive_tests
+
+enum class CognitiveTests {
+    PVT
+}
---
+++ qa_sdk/src/main/java/com/quantactions/sdk/cognitive_tests/PVTResponse.kt
@@ -0,0 +1,23 @@
+/*
+ * *******************************************************************************
+ * Copyright (C) QuantActions AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Enea Ceolini <enea.ceolini@quantactions.com>, August 2024
+ * *******************************************************************************
+ */
+
+package com.quantactions.sdk.cognitive_tests
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PVTResponse(
+    val id: String,
+    val reactionTimes: List<Long>,
+    val waitTimes: List<Long>,
+    val date: Long,
+    val localTime: String,
+    val falseStartCount: Int,
+    val noResponseCount: Int
+)
